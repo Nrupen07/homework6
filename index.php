316b84f65066a6207f86af7947cd344ea66a9686
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-11-16 06:47:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
  
 <div style="background-image: url('https://wallpaperaccess.com/full/1429585.jpg');
-background-size: cover; height:480px; padding-top: 80px; text-align: center;">
+background-size: cover; height:880px; padding-top: 80px; text-align: center;">
   <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1024px-Java_programming_language_logo.svg.png" style="height:150px; ">
   <h1 style="font-size:100px; color:white; margin:10px;">Javascript Libraries</h1>
   <p style="font-size:30px; color: white;"><em>Homework 6</em></p>
