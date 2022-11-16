b5a24d7b17be2703598b6c01d0f3586964cbd73a
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-11-16 06:06:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Javascript</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
+</head>
   <body>
 
  
+<div style="background-image: url('https://https://freerangestock.com/sample/144965/stationery-on-green-background.jpg');
+background-size: cover; height:480px; padding-top: 80px; text-align: center;">
+  <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/1024px-Java_programming_language_logo.svg.png" style="height:150px; ">
+  <h1 style="font-size:100px; color:white; margin:10px;">Javascript Libraries</h1>
+  <p style="font-size:30px; color: white;"><em>Homework 6</em></p>
+  
+</div>
 
-    <h1>It is time to use java libraries!</h1>
-   <img src="https://enosburghlibrary.files.wordpress.com/2016/02/coffee-and-books.jpg" alt="Coffee Image" style="height:500px;" /> <!-- image pointer-->
-    
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
